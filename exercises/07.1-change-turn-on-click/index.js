2212fe0b7559f88286b1076b964d4bab3a287ff8
Júlia Valls Moix <juliavalls20@gmail.com>
2023-04-20 17:05:30 +0000

    Done

--- exercises/07.1-change-turn-on-click/index.js
+++ exercises/07.1-change-turn-on-click/index.js
@@ -7,9 +7,31 @@ window.onload = function loadfn() {
 window.turnChanger = function turnChanger() {
 	if (currentUser == "Mario") {
 		currentUser = "Juan";
+	} else if (currentUser == "Juan") {
+		currentUser = "Josh";
 	} else {
 		currentUser = "Mario";
 	}
 
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
 }
+
+
+ 
+
+/* Solution according to (who knows):
+
+var currentUserIndex = 0;
+var players = ["Mario", "Juan", "Josh"];
+currentUser = players[currentUserIndex];
+
+window.onload = function loadfn() {
+	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
+}
+
+window.turnChanger = function turnChanger() {
+	currentUserIndex = (++currentUserIndex) % players.length;
+	currentUser = players[currentUserIndex];
+
+	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
+} */
--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -2,5 +2,9 @@ window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
 		//you code here
+		alert(event.target.id)
+
 	});
 };
+
+/* No seria eventInformation.target? No entenc perquè ho agafa igualment. I com pot ser que només posant .id ja agafi els ids de tot el codi html? */
